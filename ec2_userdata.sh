64d089831a22f8feee2020218737d1a959d54950
Rob <rob@ldg.net>
2014-03-18 21:57:10 -0400

    Bad copy/paste

--- ec2_userdata.sh
+++ ec2_userdata.sh
@@ -4,4 +4,4 @@ export FACTER_special_guest='Charlie Chaplin'
 
 export FACTER_launchconfig=true 
 
-/usr/bin/puppet apply --logdest /var/log/puppet_launchconfig --debug --verbose -e 'include web_server'\n",
+/usr/bin/puppet apply --logdest /var/log/puppet_launchconfig --debug --verbose -e 'include web_server'
